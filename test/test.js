3612964f12a883368e73fa3610104dd7f997179f
azu <azuciao@gmail.com>
2013-05-18 16:58:01 +0900

    init

---
+++ test/test.js
@@ -0,0 +1,32 @@
+/**
+ * User: azu
+ * Date: 2013/05/18
+ * License: MIT License
+ */
+var assert = require("assert")
+describe('Function', function (){
+    describe('When function contain /*comment*/', function (){
+        it('should contain /* comment */', function (){
+            var result = (function (){/* comment */}).toString();
+            assert.equal(result, "function (){/* comment */}");
+        });
+    });
+    describe('When function contain multiline /*comment*/', function (){
+        it('should contain /* comment */', function (){
+            var result = (function (){/*
+            comment
+            */}).toString();
+            var isContain = result.indexOf("comment") !== -1;
+            assert.ok(isContain, "result contain comment");
+        });
+    });
+    describe('When function contain //comment ', function (){
+        it('should contain /* comment */', function (){
+            var result = (function (){
+                // comment
+            }).toString();
+            var isContain = result.indexOf("// comment") !== -1;
+            assert.ok(isContain, "result contain comment");
+        });
+    });
+});
